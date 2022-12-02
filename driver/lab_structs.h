3950ba7501a18d90eb55d48fff1ef569e7d8b4aa
utsx <dimma1506@icloud.com>
2022-12-03 01:23:41 +0300

    Update

---
+++ driver/lab_structs.h
@@ -0,0 +1,17 @@
+//
+// Created by dimma on 03.12.2022.
+//
+
+#ifndef OPERATION_SYSTEM_LAB_2_LAB_STRUCTS_H
+
+struct user_pci_dev{
+    unsigned short vendor;
+    unsigned short device;
+    unsigned short subsystem_vendor;
+    unsigned short subsystem_device;
+};
+
+#define OPERATION_SYSTEM_LAB_2_LAB_STRUCTS_H
+
+
+#endif //OPERATION_SYSTEM_LAB_2_LAB_STRUCTS_H
--- driver/test.c
+++ driver/test.c
@@ -7,6 +7,7 @@
 #include <linux/proc_fs.h>
 #include <linux/init.h>
 #include <asm/uaccess.h>
+#include "lab_structs.h"
 
 #define SUCCESS 0
 #define BUF_SIZE 1024
@@ -27,7 +28,8 @@ static int lab_driver_open(struct inode *inode, struct file *file);
 static int lab_driver_release(struct inode *inode, struct file *file);
 static ssize_t lab_driver_read(struct file *file, char __user *buf, size_t count, loff_t *ppos);
 static ssize_t lab_driver_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos);
-//static *pci_dev get_pci_dev();
+static struct user_pci_dev *get_pci_dev(struct pci_dev *dev);
+static struct write_pci_dev write_pci_dev(struct user_pci_dev *dev);
 
 static struct file_operations fops = {
     .owner = THIS_MODULE,
@@ -51,11 +53,7 @@ static int lab_driver_release(struct inode *inode, struct file *file)
 static ssize_t lab_driver_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
 {
     char buffer[BUF_SIZE];
-//    size_t len = 0;
-      struct task_struct *task = get_pid_task(find_vpid(pid), PIDTYPE_PID);
-//    struct pci_dev *dev = NULL;
-//    pci_get_device(PCI_ANY_ID, PCI_ANY_ID, dev);
-//    printk(KERN_INFO "PCI_DEV \r", dev -> vendor);
+    struct task_struct *task = get_pid_task(find_vpid(pid), PIDTYPE_PID);
     printk(KERN_INFO "lab_driver: read()\r");
     if (task == NULL) {
         printk(KERN_INFO "lab_driver: task is NULL\r");
@@ -65,12 +63,33 @@ static ssize_t lab_driver_read(struct file *file, char __user *buf, size_t count
     return SUCCESS;
 }
 
+struct user_pci_dev *get_pci_dev(struct pci_dev *dev)
+{
+    struct user_pci_dev *user_dev = kmalloc(sizeof(struct user_pci_dev), GFP_KERNEL);
+    user_dev->vendor = dev->vendor;
+    user_dev->device = dev->device;
+    user_dev->subsystem_vendor = dev->subsystem_vendor;
+    user_dev->subsystem_device = dev->subsystem_device;
+    return user_dev;
+}
+
+static struct write_pci_dev write_pci_dev(struct user_pci_dev *dev) {
+    char buffer[BUF_SIZE];
+    size_t len = 0;
+    len += sprintf(buffer + len, "vendor: %d", dev->vendor);
+    len += sprintf(buffer + len, "device: %d", dev->device);
+    len += sprintf(buffer + len, "subsystem_vendor: %d", dev->subsystem_vendor);
+    len += sprintf(buffer + len, "subsystem_device: %d", dev->subsystem_device);
+    if (copy_to_user(buf, buffer, len))
+        return -EFAULT;
+    return len;
+}
+
 static ssize_t lab_driver_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
 {
     char buffer[1024];
     size_t len = 0;
     struct task_struct *task = get_pid_task(find_vpid(pid), PIDTYPE_PID);
-
     printk(KERN_INFO "lab_driver: write()\r");
     if (task == NULL) {
         printk(KERN_INFO "lab_driver: task is NULL\r");
@@ -79,12 +98,7 @@ static ssize_t lab_driver_write(struct file *file, const char __user *buf, size_
     return SUCCESS;
 }
 
-//static *pci_dev get_pci_dev(){
-//    struct pci_dev *dev = NULL;
-//    printk(KERN_INFO "lab_driver: get_pci_dev()\r");
-//    dev = pci_get_device(PCI_ANY_ID, PCI_ANY_ID, dev);
-//    return dev;
-//}
+
 
 
 static int __init lab_driver_init(void)
